d20476d943e7ef6d27242f2b4566e92e974bf7a9
kent0505 <otabekyusupov0550@gmail.com>
2025-08-16 01:52:31 +0500

    home

--- routers/home.py
+++ routers/home.py
@@ -3,6 +3,12 @@ from fastapi.templating import Jinja2Templates
 from core.security import Roles
 from db import SessionDep
 from db.user import User, db_get_users
+from db.city import db_get_cities
+from db.restaurant import db_get_restaurants
+from db.panorama import db_get_panoramas
+from db.hotspot import db_get_hotspots
+from db.category import db_get_categories
+from db.menu import db_get_menus
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
@@ -13,8 +19,12 @@ async def home(
     db: SessionDep,
 ):
     users = await db_get_users(db)
-    # cities = await db_get_cities()
-    # restaurants = await db_get_restaurants()
+    cities = await db_get_cities(db)
+    restaurants = await db_get_restaurants(db)
+    panoramas = await db_get_panoramas(db)
+    hotspots = await db_get_hotspots(db)
+    categories = await db_get_categories(db)
+    menus = await db_get_menus(db)
 
     if not users:
         user = User(
@@ -32,10 +42,11 @@ async def home(
         "index.html", {
             "request": request,
             "users": users,
-            # "cities": cities,
-            # "restaurants": restaurants,
-            # "panoramas": panoramas,
-            # "hotspots": hotspots,
-            # "menus": menus,
+            "cities": cities,
+            "restaurants": restaurants,
+            "panoramas": panoramas,
+            "hotspots": hotspots,
+            "categories": categories,
+            "menus": menus,
         }
     )
