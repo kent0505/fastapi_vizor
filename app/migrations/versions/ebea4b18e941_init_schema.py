6ae605b51075cae06a9fa828c1cbe0a9d5c24433
kent0505 <otabekyusupov0550@gmail.com>
2025-08-31 12:30:44 +0500

    1

--- app/migrations/versions/ebea4b18e941_init_schema.py
+++ app/migrations/versions/ebea4b18e941_init_schema.py
@@ -1,8 +1,8 @@
 """init schema
 
-Revision ID: 4414af0cf676
+Revision ID: ebea4b18e941
 Revises: 
-Create Date: 2025-08-26 15:06:49.895702
+Create Date: 2025-08-28 03:28:07.586072
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '4414af0cf676'
+revision: str = 'ebea4b18e941'
 down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
